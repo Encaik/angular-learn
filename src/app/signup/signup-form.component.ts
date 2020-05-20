5884f472bcdefe2184cee643ed8768f78cdf6752
Encaik <843391622@qq.com>
2020-05-20 14:22:04 +0800

    修改代码

--- src/app/signup/signup-form.component.ts
+++ src/app/signup/signup-form.component.ts
@@ -3,42 +3,8 @@ import { User } from './signup.interface';
 
 @Component({
   selector: 'signup-form',
-  template: `
-    <form (ngSubmit)="onSubmit(f)" #f="ngForm" novalidate>
-      <label>
-        <span>Full name</span>
-        <input
-          type="text"
-          name="name"
-          placeholder="Your full name"
-          required
-          [(ngModel)]="user.name">
-      </label>
-      <div ngModelGroup="account">
-        <label>
-          <span>Email address</span>
-          <input
-            type="email"
-            name="email"
-            placeholder="Your email address"
-            required
-            [(ngModel)]="user.account.email">
-        </label>
-        <label>
-          <span>Confirm address</span>
-          <input
-            type="email"
-            name="confirm"
-            placeholder="Confirm your email address"
-            required
-            [(ngModel)]="user.account.confirm">
-        </label>
-      </div>
-      <button type="submit" [disabled]="f.invalid">Sign up</button>
-      <p>{{ f.value | json }}</p>
-      <p>{{ user | json }}</p>
-    </form>
-  `
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.scss']
 })
 export class SignupFormComponent {
   public user: User = {
